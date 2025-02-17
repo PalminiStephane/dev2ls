940d1093eed64dbdc337c4275142d64c62e83261
palmini stephane <palmini.stephane@gmail.com>
2025-02-17 01:30:55 +0100

    Retouche des liens sociaux

--- public/index.php
+++ public/index.php
@@ -281,15 +281,15 @@ $recaptchaSiteKey = $_ENV['RECAPTCHA_SITE_KEY'];
                 <div class="social-links">
                     <a href="https://www.linkedin.com/in/stephane-palmini" target="_blank" rel="noopener noreferrer" class="social-link">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
-                        <span class="sr-only">LinkedIn</span>
+                        <span class="sr-only"></span>
                     </a>
                     <a href="https://www.facebook.com/profile.php?id=61571050186124" target="_blank" rel="noopener noreferrer" class="social-link">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
-                        <span class="sr-only">Facebook</span>
+                        <span class="sr-only"></span>
                     </a>
                     <a href="https://www.instagram.com/dev2ls/" target="_blank" rel="noopener noreferrer" class="social-link">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
-                        <span class="sr-only">Instagram</span>
+                        <span class="sr-only"></span>
                     </a>
                 </div>
             </div>
